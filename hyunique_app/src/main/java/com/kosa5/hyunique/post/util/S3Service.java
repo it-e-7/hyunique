61e9e10534c2056c2bf284405200d1302901484e
장서윤 <seoy316@naver.com>
2023-08-31 10:44:13 +0900

    feat: s3 이미지 일괄 삭제 기능

--- hyunique_app/src/main/java/com/kosa5/hyunique/post/util/S3Service.java
+++ hyunique_app/src/main/java/com/kosa5/hyunique/post/util/S3Service.java
@@ -1,11 +1,14 @@
 package com.kosa5.hyunique.post.util;
 
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +23,12 @@ public class S3Service {
     private final AmazonS3 amazonS3;
     private final String bucketName;
     private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
 
     public S3Service(AmazonS3 amazonS3, @Value("${cloud.aws.s3.bucket}") String bucketName, AmazonS3Client amazonS3Client) {
         this.amazonS3Client = amazonS3Client;
@@ -36,6 +45,25 @@ public class S3Service {
             String fileName = UUID.randomUUID().toString() + ".jpg";
             URL returnUrl = uploadBase64Img(base64Img, fileName);
 
+            Iterator<String> keyIterator = imgUploadState.keySet().iterator();
+
+            while (keyIterator.hasNext()) {
+                String key = keyIterator.next();
+                System.out.println("Key: " + key + ", Value: " + imgUploadState.get(key));
+            }
+//            return false;
+
+/*            // s3에 이미지 업로드를 실패한 경우
+            if (returnUrl == null) {
+
+                Iterator<String> keyIterator = imgUploadState.keySet().iterator();
+
+                while (keyIterator.hasNext()) {
+                    String key = keyIterator.next();
+                    System.out.println("Key: " + key + ", Value: " + imgUploadState.get(key));
+                }
+                return false;
+            }*/
             imgUploadState.put(fileName, returnUrl);
         }
         return true;
@@ -57,5 +85,39 @@ public class S3Service {
         }
     }
 
+    // 이미지 삭제
+    public void deleteImg(String[] files) {
 
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.AP_NORTHEAST_2)
+                .build();
+
+        try {
+            DeleteObjectsRequest dor = new DeleteObjectsRequest(bucketName).withKeys(files);
+            DeleteObjectsResult deleteObjectsResult = s3.deleteObjects(dor);
+            List<DeleteObjectsResult.DeletedObject> deletedObjects = deleteObjectsResult.getDeletedObjects();
+
+            System.out.println("Successfully deleted all the specified objects");
+
+            for (DeleteObjectsResult.DeletedObject deletedObject : deletedObjects) {
+                System.out.println("Deleted Object Key: " + deletedObject.getKey());
+            }
+
+        } catch (MultiObjectDeleteException e) {
+            System.out.println("One or more objects could not be deleted");
+            System.out.println("Error Message: " + e.getErrorMessage());
+            System.out.println("Status Code: " + e.getStatusCode());
+            System.out.println("Error Code: " + e.getErrorCode());
+            System.out.println("Request ID: " + e.getRequestId());
+
+            List<MultiObjectDeleteException.DeleteError> errors = e.getErrors();
+            for (MultiObjectDeleteException.DeleteError error : errors) {
+                System.out.println("Error: " + error.getCode() + ", Key: " + error.getKey());
+            }
+        }
+
+    }
 }
