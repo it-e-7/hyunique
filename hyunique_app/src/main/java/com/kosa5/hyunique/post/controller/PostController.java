dc597b19b5f3e876bc07fa933d046ab6434fff1a
장서윤 <seoy316@naver.com>
2023-09-01 14:10:41 +0900

    feat: 게시글 업로드 서비스 연결

--- hyunique_app/src/main/java/com/kosa5/hyunique/post/controller/PostController.java
+++ hyunique_app/src/main/java/com/kosa5/hyunique/post/controller/PostController.java
@@ -1,23 +1,22 @@
 package com.kosa5.hyunique.post.controller;
 
+import com.kosa5.hyunique.post.util.S3Service;
 import com.kosa5.hyunique.post.vo.PostProductVO;
+import com.kosa5.hyunique.post.vo.PostingVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.kosa5.hyunique.post.service.PostService;
 import com.kosa5.hyunique.post.vo.PostDetailVO;
 import com.kosa5.hyunique.post.vo.PostVO;
 
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -26,6 +25,9 @@ public class PostController {
 
     @Autowired
     PostService postService;
+
+    @Autowired
+    S3Service s3Service;
 
     @GetMapping("{postId}")
     public String getPostDetailHandler(@PathVariable("postId") int postId, Model model) {
@@ -82,9 +84,22 @@ public class PostController {
     }
 
     @PostMapping
-    public String handlePostUpload(PostVO vo) {
-        System.out.println("vo = " + vo);
-        return null;
+    @ResponseBody
+    public String handlePostUpload(@RequestBody PostingVO posting) {
+        int state = postService.uploadOnePost(posting.getPostVO(), posting.getPostProductVO());
+        System.out.println("state = " + state);
+
+        return "ok";
+    }
+
+    @GetMapping("/upload")
+    @ResponseBody
+    public String uploadFile() {
+        List<String> object_keys = Arrays.asList("post/66118058-139b-43d9-88a3-0f75f316d48f.jpg",
+                "post/685925ec-93e8-470f-a986-3855b2091d45.jpg");
+
+        s3Service.deleteImgFile(object_keys);
+        return "ok";
     }
 
 }
