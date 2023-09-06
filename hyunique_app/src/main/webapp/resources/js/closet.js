e0bdec368aac47efa2afced1d03b0136208cd74e
zzzjinwook <zzzjinwook@naver.com>
2023-09-07 00:43:52 +0900

    feat : 전체보기 구현

--- hyunique_app/src/main/webapp/resources/js/closet.js
+++ hyunique_app/src/main/webapp/resources/js/closet.js
@@ -28,6 +28,8 @@ function displayCloset(closetVO) {
         'bagList', 'dressList', 'outerList', 'topList',
         'bottomList', 'shoesList', 'hatList', 'accessoryList'
     ];
+ // allList 영역 초기화
+    $('#allList').empty();
 
     for (const category of categories) {
         let productHtml = '';
@@ -36,6 +38,9 @@ function displayCloset(closetVO) {
             productHtml += generateProductCard(product);
         }
         $(`#${category}`).html(productHtml).hide();
+
+        // allList에 미리보기 추가
+        addCategoryPreviewToAllList(category, productList);
     }
 
     // 초기 카테고리를 설정
@@ -45,7 +50,7 @@ function filterProducts(category) {
     console.log("filterProducts 함수 실행, 카테고리: " + category);
 
     const categories = [
-        'bagList', 'dressList', 'outerList', 'topList',
+        'allList', 'bagList', 'dressList', 'outerList', 'topList',
         'bottomList', 'shoesList', 'hatList', 'accessoryList'
     ];
 
@@ -56,7 +61,33 @@ function filterProducts(category) {
     currentCategory = category;
 }
 
+//새로운 함수: allList에 카테고리별 미리보기 추가
+function addCategoryPreviewToAllList(category, productList) {
+    let previewHtml = '<div class="category-preview">';
+    
+    // 이미지 4개를 묶는 div 추가
+    previewHtml += '<div class="image-wrapper">';
+    
+    let count = 0;
 
+    for (const product of productList.slice(0, 4)) {
+    	
+        previewHtml += `<img src="${product.productImg}" alt="${product.productName}" class="preview-thumbnail" />`;
+        count++;
+    }
+
+    // 아이템이 없거나 4개 미만인 경우 light-gray로 채우기
+    while (count < 4) {
+        previewHtml += '<div class="preview-thumbnail empty-thumbnail"></div>';
+        count++;
+    }
+    previewHtml += '</div>';  // 이미지 4개를 묶는 div 닫기
+
+    previewHtml += `<div class="category-name">${category}</div>`;
+    previewHtml += '</div>';
+
+    $('#allList').append(previewHtml);
+}
 
 function generateProductCard(product) {
     return `<div class="product-card" onclick="moveToProduct(${product.productId})">
