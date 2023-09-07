3075050b245ba6c9b8bf03fde2610e4f86f4a758
noino <noino0819@naver.com>
2023-09-07 11:20:38 +0900

    feat: QR Cod Scanner CSS and JavaScript update

--- hyunique_app/src/main/webapp/WEB-INF/views/readQRPage.jsp
+++ hyunique_app/src/main/webapp/WEB-INF/views/readQRPage.jsp
@@ -1,3 +1,4 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
@@ -11,6 +12,9 @@
 <body>
 	<div id="test">
 		<div id="output">
+            <button onclick="backward()">
+            			<img src="/resources/img/ic-backward.png" />
+            		</button>
 			<div id="outputMessage">
 			</div>
     		<div id="outputLayer" hidden>
@@ -18,12 +22,18 @@
     		</div>
 		</div>
 	</div>
-	<div>&nbsp;</div>
 	<div>
 		<div id="frame">
 			<div id="loadingMessage">
 			</div>
+			<div id="QRTagimg">
+			    <img src="/resources/img/QRTag.png" />
+			</div>
 			<canvas id="canvas"></canvas>
+			<div id="QRScanText">
+                옷 가격표에 있는 <br>
+                QR코드를 스캔해 주세요
+			</div>
 		</div>
 	</div>
 </body>
--- hyunique_app/src/main/webapp/resources/js/qrCamera.js
+++ hyunique_app/src/main/webapp/resources/js/qrCamera.js
@@ -1,73 +1,82 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    let passOneQR = 0;
 
-        let passOneQR = 0;
+    var video = document.createElement("video");
+    var canvasElement = document.getElementById("canvas");
+    var canvas = canvasElement.getContext("2d", { willReadFrequently: true });
+    var loadingMessage = document.getElementById("loadingMessage");
+    var outputContainer = document.getElementById("output");
+    var outputMessage = document.getElementById("outputMessage");
+    var outputData = document.getElementById("outputData");
 
-		var video = document.createElement("video");
-		var canvasElement = document.getElementById("canvas");
-		var canvas = canvasElement.getContext("2d");
-		var loadingMessage = document.getElementById("loadingMessage");
-		var outputContainer = document.getElementById("output");
-		var outputMessage = document.getElementById("outputMessage");
-		var outputData = document.getElementById("outputData");
+    function drawLine(begin, end, color) {
+        canvas.beginPath();
+        canvas.moveTo(begin.x, begin.y);
+        canvas.lineTo(end.x, end.y);
+        canvas.lineWidth = 4;
+        canvas.strokeStyle = color;
+        canvas.stroke();
+    }
 
-		function drawLine(begin, end, color) {
-			canvas.beginPath();
-			canvas.moveTo(begin.x, begin.y);
-			canvas.lineTo(end.x, end.y);
-			canvas.lineWidth = 4;
-			canvas.strokeStyle = color;
-			canvas.stroke();
+    function drawBorder() {
+        canvas.strokeStyle = "#32517F";
+        canvas.lineWidth = 5;
+        canvas.strokeRect(0, 0, canvasElement.width, canvasElement.height);
+    }
+
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function (stream) {
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true);
+        video.play();
+        requestAnimationFrame(tick);
+    });
+
+    function tick() {
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+            loadingMessage.hidden = true;
+            canvasElement.hidden = false;
+            outputContainer.hidden = false;
+            const minDimension = Math.min(video.videoWidth, video.videoHeight);
+            canvasElement.height = minDimension;
+            canvasElement.width = minDimension;
+            canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+            // 테두리 그리기 함수 호출
+            drawBorder();
+
+            var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+            var code = jsQR(imageData.data, imageData.width, imageData.height, {
+                inversionAttempts: "dontInvert",
+            });
+
+            if (code) {
+                drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF0000");
+                drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF0000");
+                drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF0000");
+                drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF0000");
+                outputMessage.hidden = true;
+                outputData.parentElement.hidden = false;
+
+                if (passOneQR == 0 && inValidURL(code.data)) {
+                    let newWindow = window.open(code.data, '_blank');
+                    passOneQR = 1;
+                    history.back();
                 }
-		navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
-      		        video.srcObject = stream;
-      		        video.setAttribute("playsinline", true);      // iOS 사용시 전체 화면을 사용하지 않음을 전달
-         		video.play();
-      		        requestAnimationFrame(tick);
-		});
-		function tick() {
-			loadingMessage.innerText = "⌛ 스캔 기능을 활성화 중입니다."
-			if(video.readyState === video.HAVE_ENOUGH_DATA) {
-        		      loadingMessage.hidden = true;
-        		      canvasElement.hidden = false;
-        		      outputContainer.hidden = false;
-        		      canvasElement.height = video.videoHeight;
-        	 	      canvasElement.width = video.videoWidth;
-        		      canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-        		      var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-        		      var code = jsQR(imageData.data, imageData.width, imageData.height, {
-                                    inversionAttempts : "dontInvert",
-        		      });
-                              if(code) { //QR코드 인식에 성공한 경우
-                                    drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF0000");
-                                    drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF0000");
-                                    drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF0000");
-                                    drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF0000");
-                                    outputMessage.hidden = true;
-                                    outputData.parentElement.hidden = false;
-                                    //코드에 있는 데이터를 출력하지만 이내 이동으로 바뀐다
-                                    if (passOneQR == 0 && inValidURL(code.data)){
-                                    let newWindow = window.open(code.data, '_blank');
-                                    passOneQR = 1;
-                                    history.back();
-                                    }
-                              }
-                              // QR코드 인식에 실패한 경우
-                              else {
-                                    outputMessage.hidden = false;
-                                    outputData.parentElement.hidden = true;
-                              }
-                      }
-      		      requestAnimationFrame(tick);
-		}
+            } else {
+                outputMessage.hidden = false;
+                outputData.parentElement.hidden = true;
+            }
+        }
+        requestAnimationFrame(tick);
+    }
 
-		//URL 로직검사
-		function inValidURL(url){
-		    let pattern  = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-		    if (pattern.test(url)){
-		        return true ;
-           }
-           else{
-                return false;
-           }
-		}
-	});
+    // URL 로직 검사 함수
+    function inValidURL(url) {
+        let pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        if (pattern.test(url)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+});
