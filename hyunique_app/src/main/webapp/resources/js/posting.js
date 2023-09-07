5f4a8b341f126903e0067cbf390a9a218ad9a42a
장서윤 <seoy316@naver.com>
2023-09-07 17:05:49 +0900

    feat: 사이즈, 색상 선택 모달 서버 연결

--- hyunique_app/src/main/webapp/resources/js/posting.js
+++ hyunique_app/src/main/webapp/resources/js/posting.js
@@ -9,6 +9,9 @@ let container;
 
 let items = {};
 
+let imgWidth;
+let imgHeight;
+
 $(document).ready(function() {
 
     getTagInform();
@@ -27,7 +30,7 @@ $(document).ready(function() {
     // 작성 완료 버튼
     $('#upload-button').click(compileAndSendPostData);
 
-
+    // 입력 이미지 출력
     $("#fileInput").change(function(e) {
         const files = e.target.files;
         $.each(files, function(index, file) {
@@ -36,6 +39,7 @@ $(document).ready(function() {
                 return;
             }
             const reader = new FileReader();
+
             reader.onload = function(e) {
                 const imageElement = $("<img>").attr("src", e.target.result).attr("data-file", file.name);
                 const img = reader.result.split(',')[1];
@@ -44,29 +48,127 @@ $(document).ready(function() {
                 const li = $("<li>").append(imageElement);
                 $('#image-list').append(li);
                 container = imageElement;
-
-                imageElement.click(function(e) {
-                    let XOffset = e.offsetX;
-                    let YOffset = e.offsetY;
-
-                    $(".result-list").empty();
-                    $(".write-container").hide();
-                    $(".search-container").show();
-
-                    $("#search-btn").off('click').click(attachTag(XOffset, YOffset, li));
-                });
-            };
+            }
             reader.readAsDataURL(file);
         });
     });
+
+    // 사용자가 클릭한 좌표 정보 저장
+    let XOffset = 0;
+    let YOffset = 0;
+
+    $('#image-list').on('click', 'li:first-child img', function(e){
+        imgWidth = $(this).width();
+        imgHeight = $(this).height();
+
+        XOffset = e.offsetX;
+        YOffset = e.offsetY;
+
+        $(".result-list").empty();
+        $(".write-container").hide();
+        $(".search-container").show();
+    });
+
+    // 상품 검색
+    $("#search-btn").off('click').click(function(){
+        let productName = $("#search-input").val();
+
+        if(productName){
+          getSearchProduct(productName);
+        }
+    });
+
+    $('.result-list').on('click', '.search-product-li', function() {
+        vo = getSelectItem($(this));
+        showProductModal(vo);
+    });
+
+
+    $("#search-results-button").click(function() {
+        attachTag(XOffset, YOffset, li)();
+    });
+
+
+
+    // 새로운 코드 시작
+    let colorIndex = 0;
+    let sizeIndex = 0;
+    const colorItems = $('#color-picker .slide-item');
+    const sizeItems = $('#size-picker .slide-item');
+    const colorCount = colorItems.length;
+    const sizeCount = sizeItems.length;
+    let initialTouchPos = null;
+    let lastTouchPos = null;
+
+    function updateSlide(newTop = null, pickerId) {
+      if (pickerId === 'color-picker') {
+        if (newTop === null) {
+          newTop = -colorIndex * 50;
+        }
+        $('#colorContent').css('top', `${newTop}px`);
+      } else if (pickerId === 'size-picker') {
+        if (newTop === null) {
+          newTop = -sizeIndex * 50;
+        }
+        $('#sizeContent').css('top', `${newTop}px`);
+      }
+    }
+
+    updateSlide(null, 'color-picker');
+    updateSlide(null, 'size-picker'); // 초기 위치 설정
+
+    $('#color-picker, #size-picker').on('touchstart', function(e) {
+      initialTouchPos = e.originalEvent.touches[0].clientY;
+    });
+
+    $('#color-picker, #size-picker').on('touchmove', function(e) {
+      e.preventDefault();
+      lastTouchPos = e.originalEvent.touches[0].clientY;
+      const offset = lastTouchPos - initialTouchPos;
+      const newTop = this.id === 'color-picker' ? -colorIndex * 50 + offset : -sizeIndex * 50 + offset;
+      updateSlide(newTop, this.id);
+    });
+
+    $('#color-picker, #size-picker').on('touchend', function(e) {
+      const finalTouchPos = e.originalEvent.changedTouches[0].clientY;
+
+      if (initialTouchPos - finalTouchPos > 30) {  // Upward swipe
+        if (this.id === 'color-picker') {
+          colorIndex = (colorIndex + 1) % colorCount;
+        } else {
+          sizeIndex = (sizeIndex + 1) % sizeCount;
+        }
+      } else if (initialTouchPos - finalTouchPos < -30) { // Downward swipe
+        if (this.id === 'color-picker') {
+          colorIndex = (colorIndex - 1 + colorCount) % colorCount;
+        } else {
+          sizeIndex = (sizeIndex - 1 + sizeCount) % sizeCount;
+        }
+      }
+      updateSlide(null, this.id);
+    });
+    // 새로운 코드 끝
 });
 
+// 검색 결과 리스트에서 선택한 아이템 정보 저장해서 객체로 반환
+function getSelectItem(obj) {
+
+    itemVO = {
+        productId : obj.find(".search-product-id").text(),
+        productBrand : obj.find(".search-product-brand").text(),
+        productName : obj.find(".search-product-name").text(),
+        productSize : '',
+        productColor : '',
+    };
+
+    return itemVO;
+}
 
 // 게시글 내용 업로드
 function compileAndSendPostData() {
     let tagValues = getGroupCheckBoxState();
 
-    let PostVO = {
+    let post = {
         postContent: $('#content').val(),
         tpoId: -1,
         seasonId: -1,
@@ -86,14 +188,26 @@ function compileAndSendPostData() {
         let tagNames = Object.keys(tagValueObj);
         let tagIds = Object.values(tagValueObj);
 
-        PostVO[groupName] = tagIds;
+        post[groupName] = tagIds;
         nextPost[groupName] = tagNames
     });
 
-    PostVO['seasonId'] = PostVO['seasonId'][0];
-    PostVO['tpoId'] = PostVO['tpoId'][0];
+    post['seasonId'] = post['seasonId'][0];
+    post['tpoId'] = post['tpoId'][0];
 
-    sendPostToServer(PostVO, items);
+    // 핀
+    let product = Object.values(items).map(item => {
+        return {
+            pinX: item.xOffset / imgWidth,
+            pinY: item.yOffset / imgHeight,
+            productId: item.productId,
+            productSize: item.productSize,
+        };
+    });
+
+    let postingVO = {postVO: post, postProductVO: product};
+
+    sendPostToServer(postingVO);
     printSelectTagAndContent(nextPost);
 
     $(".post-container").show();
@@ -104,76 +218,123 @@ function compileAndSendPostData() {
 // 터치한 위치에 선택한 상품을 태그로 붙이기
 function attachTag(xOffset, yOffset, li) {
     return function() {
-        let tagValue = $("#search-input").val();
 
-        if (tagValue) {
-            getSearchProduct(tagValue);
+        let tagElement = $("<span>").addClass("tag").attr("id","tag_"+new Date().getTime()).css({
+            left: xOffset + "px",
+            top: yOffset + "px",
+            position: "absolute"
+        });
 
-            let tagElement = $("<span>").addClass("tag").attr("id","tag_"+new Date().getTime()).css({
-                left: xOffset + "px",
-                top: yOffset + "px",
-                position: "absolute"
-            });
+        let id = tagElement.attr('id');
 
-            let id = tagElement.attr('id');
-            items[id] = {
-                initialX: 0,
-                initialY: 0,
-                currentX: 0,
-                currentY: 0,
-                xOffset: 0,
-                yOffset: 0,
-                active: false,
-                productId: '',
-                productBrand: '',
-                productName: '',
-                productPrice: 0,
-                productSize: '',
-                productColor: ''
+        items[id] = {
+            initialX: 0,
+            initialY: 0,
+            currentX: 0,
+            currentY: 0,
+            xOffset: 0,
+            yOffset: 0,
+            active: false,
+            productId: '',
+            productBrand: '',
+            productName: '',
+            productPrice: 0,
+            productSize: '',
+            productColor: ''
+        };
 
-            };
 
-            $(".result-list").off("click").on("click", ".search-product-li", function() {
-                items[id].productId = $(this).find(".search-product-id").text();
-                items[id].productBrand = $(this).find(".search-product-brand").text();
-                items[id].productName = $(this).find(".search-product-name").text();
-                items[id].productPrice = $(this).find(".search-product-price").text();
-                items[id].productSize = $(this).find(".search-product-size").text();
+        $(".result-list").off("click").on("click", ".search-product-li", function() {
+            items[id].productId = $(this).find(".search-product-id").text();
+            items[id].productBrand = $(this).find(".search-product-brand").text();
+            items[id].productName = $(this).find(".search-product-name").text();
 
-                console.log(id, items[id]);
+            items[id].productSize = $("#sizeContent .slide-item.active").text();
+            items[id].productColor = $("#colorContent .slide-item.active").text();
 
-                tagElement.html(`
-                  ${items[id].productBrand}
-                  ${items[id].productName}
-                  ${items[id].productPrice}
-                  ${items[id].productSize}
-                `);
+            console.log(id, items[id]);
 
-                $(".search-container").hide();
-                $(".write-container").show();
-                $("#search-input").val("");
+            tagElement.html(`
+              ${items[id].productBrand}
+              ${items[id].productName}
+              ${items[id].productPrice}
+              ${items[id].productSize}
+            `);
 
-            });
+            $('.modal').hide();
+            $(".search-container").hide();
+            $(".write-container").show();
+            $("#search-input").val("");
 
-            items[id].xOffset = xOffset;
-            items[id].yOffset = yOffset;
+        });
 
-            li.append(tagElement);
+        items[id].xOffset = xOffset;
+        items[id].yOffset = yOffset;
 
-            // 새로 생성된 태그에 드래그 이벤트 바인딩
-            tagElement.on("mousedown touchstart", function(event) {
-                dragStart(event);
-            });
+        li.append(tagElement);
 
-            tagElement.on("mouseup touchend", function(event) {
-                dragEnd(event);
-            });
+        // 새로 생성된 태그에 드래그 이벤트 바인딩
+        tagElement.on("mousedown touchstart", function(event) {
+            dragStart(event);
+        }, { passive: true });
 
-            tagElement.on("mousemove touchmove", function(event) {
-                drag(event, tagElement);
-            });
-        }
+        tagElement.on("mouseup touchend", function(event) {
+            dragEnd(event);
+        }, { passive: true });
+
+        tagElement.on("mousemove touchmove", function(event) {
+            drag(event, tagElement);
+        }, { passive: true });
     };
+}
+
+function showProductModal(product) {
+    $("#product-info").text('사이즈 색상');
+    let modal = $("#product-search-modal");
+
+    console.log(product['productId']);
+
+    requestProductSizeAndColor(product['productId']);
+
+    modal.show();
+
+    $(".close-button").click(function() {
+        modal.hide();
+        $("#sizeContent").empty();
+        $("#colorContent").empty();
+    });
+
+    $('#search-results-button').click(function() {
+
+    });
+}
+
+// 상품 사이즈, 색상 DB에서 조회
+function requestProductSizeAndColor(productId) {
+    $.getJSON(`/product/inform?productId=${productId}`, function(data) {
+        // 기존 내용 삭제
+
+        // size 처리
+        if (data.productSize.length > 0) {
+            $.each(data.productSize, function(index, size) {
+                const slideItem = $("<div>").addClass("slide-item").text(size);
+                $("#sizeContent").append(slideItem);
+            });
+        } else {
+            console.log("No sizes available");
+        }
+
+        // color 처리
+        if (data.productColor.length > 0) {
+
+            $.each(data.productColor, function(index, color) {
+                const slideItem = $("<div>").addClass("slide-item").text(color);
+                $("#colorContent").append(slideItem);
+            });
+        } else {
+            console.log("No colors available");
+        }
+    });
 }
 
 function dragStart(event) {
@@ -241,24 +402,12 @@ function setTranslate(xPos, yPos, el) {
     });
 }
 
-function sendPostToServer(post, product) {
-    const productArray = Object.values(items).map(item => {
-        return {
-            pinX: item.xOffset,
-            pinY: item.yOffset,
-            productId: item.productId,
-            productSize: item.productSize,
-        };
-    });
-
-    let combineObject = {postVO: post, postProductVO: productArray};
-    console.log('test: ' + combineObject);
-
+function sendPostToServer(postingVO) {
     $.ajax({
         url: `/post`,
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify(combineObject),
+        data: JSON.stringify(postingVO),
         success: function(response) {
             console.log(response);
         },
@@ -274,22 +423,33 @@ function getSearchProduct(productName) {
         type: 'GET',
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         success: function(response) {
-            let resultList = $(".result-list");
-            $('.search-value').val(productName);
-
-            $.each(response, function(index, product) {
-                let listItem = $("<li>").addClass("search-product-li");
-                let divItem = $("<div>").addClass("search-product-div");
-                listItem.append($("<img>").attr("src", product.productImg).addClass("search-product-img"));
-                divItem.append($("<p>").text(product.productId).addClass("search-product-id").attr("hidden", true));
-                divItem.append($("<p>").text(product.productBrand).addClass("search-product-brand"));
-                divItem.append($("<p>").text(product.productName).addClass("search-product-name"));
-                divItem.append($("<p>").text(product.productPrice).addClass("search-product-price"));
-                divItem.append($("<p>").text(product.productSize).addClass("search-product-size"));
-                listItem.append(divItem);
-                resultList.append(listItem);
-            });
+            renderSearchProductResults(response, productName);
         }
+    });
+}
+
+// 상품 검색 결과 출력
+function renderSearchProductResults(results, productName) {
+
+    let resultList = $(".result-list");
+    $('.search-value').text(`${productName} 검색 결과`);
+
+    $.each(results, function(index, product) {
+        let listItem = $("<li>").addClass("search-product-li");
+        let divItem = $("<div>").addClass("search-product-div");
+        listItem.append($("<img>").attr("src", product.productImg).addClass("search-product-img"));
+        divItem.append($("<p>").text(product.productId).addClass("search-product-id").attr("hidden", true));
+        divItem.append($("<p>").text(product.productBrand).addClass("search-product-brand"));
+        divItem.append($("<p>").text(product.productName).addClass("search-product-name"));
+        divItem.append($("<p>").text(product.productPrice).addClass("search-product-price"));
+        listItem.append(divItem);
+
+//        listItem.click(function() {
+//            // 상품 클릭하면 해당 상품의 번호를 조회해서 모달 띄움
+//            showProductModal(product);
+//        })
+
+        resultList.append(listItem);
     });
 }
 
@@ -301,7 +461,7 @@ function handleCheckBoxState(tag, groupClass) {
     console.log("checkbox: ", checkbox);
 }
 
-// 각 그룹별로 체크된 체크박스의 값을 ,단위로 구분해서 하나의 String 으로 반환
+// 각 그룹별로 체크된 체크박스의 값을 Map<TagName, TagId> 로 반환
 function getCheckedValuesInGroup(groupClass, type) {
     let checkedValuesMap = {};
 
@@ -354,6 +514,15 @@ function insertTags(tagType, tagData) {
         $tagContainer.append(tagElement);
     });
 }
+
+function getProductSizeAndColor() {
+    $.getJSON("/post/tag", function(data) {
+        insertTags('style', data.styleTags);
+        insertTags('tpo', data.tpoTags);
+        insertTags('season', data.seasonTags);
+    });
+}
+
 
 function printSelectTagAndContent(vo) {
     let tpoIdStr = '#' + vo['tpoId'][0];
