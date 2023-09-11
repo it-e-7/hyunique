d3f846d99fef223d9f606f268f258c2874199bbe
noino <noino0819@naver.com>
2023-09-11 17:11:01 +0900

    fix: session Id error

--- hyunique_app/src/main/java/com/kosa5/hyunique/HomeController.java
+++ hyunique_app/src/main/java/com/kosa5/hyunique/HomeController.java
@@ -1,11 +1,13 @@
 package com.kosa5.hyunique;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.kosa5.hyunique.post.service.PostService;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
@@ -14,10 +16,10 @@ public class HomeController {
 	PostService postService;
 	
 	@GetMapping(value = "/")
-    public String getPostingListHandler(@SessionAttribute("sessionId") Integer sessionId,Model model) {
-        model.addAttribute("userId",sessionId);
-        if(sessionId!=null){
-            model.addAttribute("followerCount",postService.countFollower(sessionId));
+    public String getPostingListHandler(HttpSession session,Model model) {
+        model.addAttribute("userId",session.getAttribute("sessionId"));
+        if(session.getAttribute("sessionId")!=null){
+            model.addAttribute("followerCount",postService.countFollower(Integer.parseInt((String)session.getAttribute("sessionId"))));
         }
         return "postList";
     }
--- hyunique_app/src/main/java/com/kosa5/hyunique/post/controller/FilterController.java
+++ hyunique_app/src/main/java/com/kosa5/hyunique/post/controller/FilterController.java
@@ -3,6 +3,8 @@ package com.kosa5.hyunique.post.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.kosa5.hyunique.post.service.PostService;
 import com.kosa5.hyunique.post.vo.FilterPostVO;
 import com.kosa5.hyunique.post.vo.PostVO;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequestMapping ("/filter")
@@ -23,9 +24,10 @@ public class FilterController {
 	private PostService postService;
 
 	@GetMapping(value = "getFilterPost")
-	public String getFilterPost(@SessionAttribute("sessionId") Integer sessionId , @ModelAttribute FilterPostVO filterPostVO, Model model) {
+	public String getFilterPost(HttpSession session, @ModelAttribute FilterPostVO filterPostVO, Model model) {
 		List<PostVO> postVOList = new ArrayList<>();
-		if(sessionId!=null){
+		if(session.getAttribute("sessionId")!=null){
+			Integer sessionId = Integer.parseInt((String)session.getAttribute("sessionId"));
 			filterPostVO.setUserId(sessionId);
 		}
 		postVOList = postService.getfilterPostList(filterPostVO);
