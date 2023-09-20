77cc1a8781029f214d313d20cccbdac0a4d889f4
noino <noino0819@naver.com>
2023-09-20 10:25:31 +0900

    feat: Ai system with product list

--- hyunique_app/src/main/webapp/resources/js/gpt.js
+++ hyunique_app/src/main/webapp/resources/js/gpt.js
@@ -102,10 +102,10 @@ function gptProductRequest (){
             		    	    			<div class="purchase-cancel-btn">
             		    	    				다음에 구매할게요
             		    	    			</div>
-          	    	    				<div class="purchase-accept-btn">
+          	    	    				<button class="purchase-accept-btn" onclick="paymentToss()">
             		    	    				눌러서 구매 완료
             		    	    			</div>
-            		    	    		</div>`
+            		    	    		</button>`
             		    	    )
       },
       error: function(error) {
