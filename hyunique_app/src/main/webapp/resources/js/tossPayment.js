695332abe5d7e5ad6206af361b6ef7c1514a20ed
noino <noino0819@naver.com>
2023-09-23 17:24:41 +0900

    feat: 안체크된 이미지에서 체크된 이미지만 정송으로 수정

--- hyunique_app/src/main/webapp/resources/js/tossPayment.js
+++ hyunique_app/src/main/webapp/resources/js/tossPayment.js
@@ -34,7 +34,7 @@ const productListElements = document.querySelectorAll('.gpt-product-list');
 let orderList = [];
 productListElements.forEach(productElement => {
    //현재는 테스트를 위하여 체크되지 않은 이미지를 전송합니다. 이후에는 수정해주세요
-  const imageSource = productElement.querySelector('img[src="/resources/img/ic-bag-non-check.png"]').getAttribute('src');
+  const imageSource = productElement.querySelector('img[src="/resources/img/ic-bag-check.png"]').getAttribute('src');
   if (imageSource == '/resources/img/ic-bag-non-check.png') {
     const productWrapper = productElement.querySelector('#product-only-wrapper');
     productWrapper.getAttribute("onclick");
